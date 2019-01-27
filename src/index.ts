4f079bcfb6bf159c015367c5127731f343c93ad7
Charles Peden <ccbpeden@warpmail.net>
2019-01-27 06:18:14 -0800

    require environmental variables at root

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { GraphQLServer } from 'graphql-yoga'
 import { prisma } from './generated/prisma-client'
 import resolvers from './resolvers'
+const env = require('dotenv').config();
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
